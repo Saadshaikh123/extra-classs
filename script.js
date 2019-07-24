b673d6fb627386a70ec854ff8e13562b32efef13
Muhammad Saad <49517382+Saadshaikh123@users.noreply.github.com>
2019-07-24 17:47:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,146 @@
+/* Data Information
+- number (age, students, height, weight, matrik marks,
+          dozen of fruits)
+
+/*boolean (aesi information jska jwb han ya na mein ho..(uska jwb mein true or false aay)
+            exm: isClassTomorrow, isHungry, isExams, isGoodStudents)
+examples:-
+let isClassTomorrow;
+isClassTOmorrow = false;
+let isHungry = true;
+let exams = false; */
+
+
+/*string (word ya sentence...(name, fvrtDish, researchTopic etc...))
+three ways to write string ("", '', ``)
+
+examples:-
+let name = "saad";
+let discription = "something peragraph"
+let name2 = "arsalan";
+let topic = "pubg";
+topic = "subway surfers" */
+
+
+/* variable
+-variable - ak container jis k ander hum change kr skty hein
+-let glass "pepse" //initialization;
+-glass= "water";
+-glass= "juice";
+
+---numberExample
+let age = 18;
+-age = 19;
+-age = 20;
+
+let numberOfStudent;
+numberOfStudents = 70;
+let height = 70;
+let marks = 90;
+let matrikPercentage = 80;
+
+human naming rules:-
+-number nh askty
+-sirf alphabates
+- no special chracter (*&%$@/.)
+
+varibale naming rules:-
+-numbers ALLOWED
+-lekin number se shuru nh hota
+-spaces not allowed
+-special chracter not allowed
+-two special chracter allowed _$
+-variable naming should be use lowerCase
+
+casing(how to write words together without space):-
+-camel case: saadAshqeen
+-snake case: saad_Asheqeen_
+-kabab case: saad-ashqeen
+-
+*/
+
+
+/* decision
+if(){} :- decision making
+
+-agr temperature zyada hua to me aunga wrna nhe.
+-let temperature = 36;
+-if (temperature > 40) {
+    console.log ("not comming")
+} else {
+    console.log ("comming")
+}
+
+-agr deadline kl he toh rat jagna he awrna nhe.
+- let isDEadLineTomorrow = true;
+if (isDEadLineTomorrow == true){
+    console.log ("jagna he")
+} else {
+    console.log ("nahi jagna");
+}
+
+-agr distance kam he to 1km se to walk kro
+wrna agr 10 se kam he to bus se jao warna careem
+
+let distance = 0.5;
+
+if(distance < 1){
+    console.log("walking")
+}else if (distance < 10){
+    console.log("bus me jao")
+} else {
+    console.log ("careem")
+}
+
+let marks = 70;
+ if(marks > 90){
+    console.log("A");
+ } else if{("marks > 80")
+   console.log("B")
+}  else if{("marks > 60");
+   console.log("C")
+}else if{("marks > 40");
+   console.log("D")
+}else if{("marks > 30");
+   console.log("E")
+}else {
+   console.log("FAIL");
+}
+*/
+
+/* LOOPS -repetation khatam karne k lye
+-for loop
+-while loop
+-for each loop
+-for of
+-for in
+-do while
+
+for(){} :- avoid retaion
+
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+console.log("calp!");
+this repeatation khtma krne k lye ww use 
+for (let i = 0; i < 10; i++) {
+    console.log("Clap")
+}
+
+// -counting 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// counting 50 to 1
+for (let i = 50; i >= 1; i--) {
+    console.log(i);
+}*/
